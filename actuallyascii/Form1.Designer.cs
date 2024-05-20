0a58cd16ff5cc941f45d6d5bfcc5bffaf931c444
abelabelabel <szaboabel2006@gmail.com>
2024-05-20 21:11:01 +0200

    Add project files.

---
+++ actuallyascii/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace actuallyascii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_search = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_ascii = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(62, 29);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(93, 50);
+            this.btn_search.TabIndex = 0;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_ascii
+            // 
+            this.btn_ascii.Location = new System.Drawing.Point(360, 29);
+            this.btn_ascii.Name = "btn_ascii";
+            this.btn_ascii.Size = new System.Drawing.Size(107, 50);
+            this.btn_ascii.TabIndex = 2;
+            this.btn_ascii.Text = "ASCII";
+            this.btn_ascii.UseVisualStyleBackColor = true;
+            this.btn_ascii.Click += new System.EventHandler(this.btn_ascii_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_ascii);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_ascii;
+    }
+}
+
---
+++ actuallyascii/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace actuallyascii
+{
+    public partial class Form1 : Form
+    {
+        Img image;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Select file";
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "png|*.png";
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string s = ofd.FileName;
+                textBox1.Text = s.Substring(s.LastIndexOf("\\") + 1);
+                Bitmap bitmap = new Bitmap(ofd.FileName);
+                image = new Img(bitmap);
+
+                
+            }
+        }
+
+        private void btn_ascii_Click(object sender, EventArgs e)
+        {
+            string text = image.ascii();
+            using (StreamWriter sw = new StreamWriter(".\\out.txt"))
+            {
+                sw.Write(text);
+            }
+            MessageBox.Show("OK");
+
+        }
+    }
+
+    public class Img
+    {
+        private Bitmap bitmap;
+
+        public Img(Bitmap img)
+        {
+            bitmap = img;
+        }
+
+        public string ascii()
+        {
+            string s = "";
+
+            double brightness;
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixel = bitmap.GetPixel(x, y);
+                    brightness = pixel.R * 0.299 + pixel.G * 0.587 + pixel.B * 0.114;
+
+                    if (brightness > 0 && brightness <= 50) s += "&";
+                    else if (brightness > 50 && brightness <= 100) s += "\\";
+                    else if (brightness > 100 && brightness <= 150) s += "$";
+                    else if (brightness > 150 && brightness <= 200) s += "%";
+                    else if (brightness > 200 && brightness <= 255) s += "@";
+                }
+
+                s += "\n";
+            }
+
+            return s;
+        }
+    }
+}
